3b0088416dbdf7574e7f7d2f6b729a3f58bc73f2
bzgoering <bzgoering@icloud.com>
2024-12-10 00:42:50 -0600

    Update Chip.java
    keeps infomation about each chip(circle) togeather.

--- Chip.java
+++ Chip.java
@@ -1,1 +1,32 @@
-//coding in progress
+import javafx.scene.shape.Circle;
+
+public class Chip
+{
+	private Circle circle;
+	private String occupy;
+	private boolean alive;
+	private boolean king;
+
+	public Chip(Circle circ, String oc)
+	{
+		alive = true;
+		occupy = oc;
+		circle = circ;
+		king = false;
+	}
+	
+	public Circle getCircle()
+	{
+		return circle;
+	}
+	
+	public String getTile()
+	{
+		return occupy;
+	}
+	
+	protected void setTile(String rec)
+	{
+		occupy = rec;
+	}
+}
