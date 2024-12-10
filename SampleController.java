59e6c1804a4986a77715a7b81cfe4e35f0ac918a
bzgoering <bzgoering@icloud.com>
2024-12-10 00:45:26 -0600

    Update SampleController.java

--- SampleController.java
+++ SampleController.java
@@ -1,1 +1,71 @@
+public class SampleController implements Initializable
+{	
+	//Control
+		//label
+		@FXML private Label turnTxt;
+		//pane
+		@FXML private Pane CheckerBoard;
+		//buttons
+		@FXML private Button ResetButton;
+		@FXML private Button startUp;
+		//blue chips
+		@FXML private Circle Blue1, Blue5, Blue9, Blue2, Blue6, Blue10, Blue3, Blue7, Blue11, Blue4, Blue8, Blue12;
+		//red chips
+		@FXML private Circle Red1, Red5, Red9, Red2, Red6, Red10, Red3, Red7, Red11, Red4, Red8, Red12;
+	
+	//Lists
+		protected static List<Chip>RedChips = new ArrayList<>();
+		protected static List<Chip>BlueChips = new ArrayList<>();
+		
+	//adds blue chips to list
+	private void addBlue()
+	{
+		BlueChips.add(new Chip(Blue1,"A1"));	BlueChips.add(new Chip(Blue5,"H2"));	BlueChips.add(new Chip(Blue9,"A3"));	
+		BlueChips.add(new Chip(Blue2,"C1"));	BlueChips.add(new Chip(Blue6,"F2"));	BlueChips.add(new Chip(Blue10,"C3"));	
+		BlueChips.add(new Chip(Blue3,"E1"));	BlueChips.add(new Chip(Blue7,"D2")); 	BlueChips.add(new Chip(Blue11,"E3"));	
+		BlueChips.add(new Chip(Blue4,"G1"));	BlueChips.add(new Chip(Blue8,"B2"));	BlueChips.add(new Chip(Blue12,"G3"));
+	}
+	
+	//adds red chips to list
+	private void addRed()
+	{
+		RedChips.add(new Chip(Red1,"B8"));	RedChips.add(new Chip(Red5,"G7"));	RedChips.add(new Chip(Red9,"B6"));	
+		RedChips.add(new Chip(Red2,"D8"));	RedChips.add(new Chip(Red6,"E7"));	RedChips.add(new Chip(Red10,"D6"));	
+		RedChips.add(new Chip(Red3,"F8"));	RedChips.add(new Chip(Red7,"C7"));	RedChips.add(new Chip(Red11,"F6"));	
+		RedChips.add(new Chip(Red4,"H8"));	RedChips.add(new Chip(Red8,"A7"));	RedChips.add(new Chip(Red12,"H6"));
+	}
+	
+  	// Reset scene method 
+ 	public void resetScene(ActionEvent Event) 
+ 	{ 
+ 		try 
+ 		{ 
+ 			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml")); 
+ 			Parent root = loader.load(); 
+ 			Stage stage = (Stage) CheckerBoard.getScene().getWindow(); 
+ 			stage.setScene(new Scene(root)); 
+ 		} 
+ 		catch (Exception e) 
+ 		{ 
+ 			e.printStackTrace(); 
+ 		}
+ 	}
+ 	
+ 	//on startup
+ 	@Override
+ 	public void initialize(URL url, ResourceBundle RB) 
+ 	{
+ 		//sets variables at start up
+ 		addRed();
+ 		addBlue();
 
+ 		//starts game
+ 		startGame();
+ 	}
+ 	
+ 	private void startGame()
+ 	{
+ 		System.out.println("Starting...");
+ 		Draggable draggable = new Draggable(turnTxt);
+ 	}
+}
