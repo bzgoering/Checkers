7022041c024cee334a8cc75fc5db68fe8d10e957
bzgoering <bzgoering@icloud.com>
2024-12-10 22:33:12 -0600

    Update Chip.java

--- Chip.java
+++ Chip.java
@@ -1,15 +1,15 @@
+package application;
+
 import javafx.scene.shape.Circle;
 
 public class Chip
 {
 	private Circle circle;
 	private String occupy;
-	private boolean alive;
 	private boolean king;
 
 	public Chip(Circle circ, String oc)
 	{
-		alive = true;
 		occupy = oc;
 		circle = circ;
 		king = false;
