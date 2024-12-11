d182b0387dd4b9b4c3cb7123ba393485bd1483b6
bzgoering <bzgoering@icloud.com>
2024-12-10 19:44:36 -0600

    Update SampleController.java
    add debug/log file and the ability to open it.

--- SampleController.java
+++ SampleController.java
@@ -1,3 +1,19 @@
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.*;
+import javafx.stage.Stage;
+
 public class SampleController implements Initializable
 {	
 	//Control
@@ -7,15 +23,19 @@ public class SampleController implements Initializable
 		@FXML private Pane CheckerBoard;
 		//buttons
 		@FXML private Button ResetButton;
-		@FXML private Button startUp;
+		@FXML private Button DebugButton;
 		//blue chips
 		@FXML private Circle Blue1, Blue5, Blue9, Blue2, Blue6, Blue10, Blue3, Blue7, Blue11, Blue4, Blue8, Blue12;
 		//red chips
 		@FXML private Circle Red1, Red5, Red9, Red2, Red6, Red10, Red3, Red7, Red11, Red4, Red8, Red12;
 	
+		
 	//Lists
 		protected static List<Chip>RedChips = new ArrayList<>();
 		protected static List<Chip>BlueChips = new ArrayList<>();
+
+	//Debug stuff
+		Debug debug = new Debug();
 		
 	//adds blue chips to list
 	private void addBlue()
@@ -58,14 +78,20 @@ public class SampleController implements Initializable
  		//sets variables at start up
  		addRed();
  		addBlue();
-
+ 		
  		//starts game
  		startGame();
+ 		
  	}
  	
  	private void startGame()
  	{
- 		System.out.println("Starting...");
+ 		debug.write("Starting...\n");
  		Draggable draggable = new Draggable(turnTxt);
+ 	}
+ 	
+ 	public void debugButton(ActionEvent Event)
+ 	{
+ 		debug.openFile();
  	}
 }
